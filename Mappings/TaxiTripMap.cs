c9c2e2d08f9a3fd5840869b43d81d0ce0ff6364c
Volodymyr_Mykhailiv <volodymyrmykhailiv2005@gmail.com>
2026-02-21 14:10:00 +0200

    Add mapping and csv files

---
+++ Mappings/TaxiTripMap.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiData_ETL_Processor.Models;
+
+namespace TaxiData_ETL_Processor.Mappings
+{
+    public class TaxiTripMap : ClassMap<TaxiTrip>
+    {
+        public TaxiTripMap()
+        {
+            AutoMap(CultureInfo.InvariantCulture);
+
+            Map(m => m.TpepPickupDatetime).Name("tpep_pickup_datetime");
+            Map(m => m.TpepDropoffDatetime).Name("tpep_dropoff_datetime");
+            Map(m => m.PassengerCount).Name("passenger_count");
+            Map(m => m.TripDistance).Name("trip_distance");
+            Map(m => m.StoreAndFwdFlag).Name("store_and_fwd_flag");
+            Map(m => m.PULocationID).Name("PULocationID");
+            Map(m => m.DOLocationID).Name("DOLocationID");
+            Map(m => m.FareAmount).Name("fare_amount");
+            Map(m => m.TipAmount).Name("tip_amount");
+        }
+    }
+}
